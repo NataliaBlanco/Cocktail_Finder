9a390b9023db895fa0cbfae66fdacf656e204dca
Natalia Blanco <nataliaouteiral@gmail.com>
2023-02-23 19:41:16 +0100

    added  variables to js and deleted some unused files

---
+++ public/assets/js/main.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/* Instructions
+ *1- Crear Una esctructura Basica de HTML dividida en sus correspondientes ficheros separados
+ *2- Crear Constantes con las que se va a trabajar
+ *3-Crear esctructura que quiero que se me pinte en JS
+ *4-traer los datos con los que se van a trabajar, comprobar que están bien y que los traen bien de la API
+ *5-
+ *6-
+ *7
+ *8-
+ */
+
+const inputCocktail = document.querySelector('.js-input-cocktail');
+const btnSearch = document.querySelector('.js-btn-search');
+const btnReset = document.querySelector('.js-btn-reset');
+const cocktailList = document.querySelector('.js-list');
+const cocktailFavList = document.querySelector('.js-favourites-list');
+
+'use strict';
+
+'use strict';
+
+console.log('>> Ready :)');
+
+//# sourceMappingURL=main.js.map
