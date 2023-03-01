68556ca519341d9e862bfe6e762d8d728a5dc081
Natalia Blanco <nataliaouteiral@gmail.com>
2023-03-01 09:39:28 +0100

    tests & trials for functions

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -128,7 +128,8 @@ function PaintFavCocktails(listFavCocktailData) {
 function handleClickBtnReset(ev) {
   ev.preventDefault();
   cocktailFavList.innerHTML = '';
-  cocktailList.innerHTML += PaintCocktail(cocktail, false);
+  cocktailList.innerHTML = '';
+
   /* ev.currentTarget. classList.remove('selected_cards'); */
 }
 
